55add2a519c8c45bea5432cef4319089e4337b4b
devgeek <nehasinghpro2700@gmail.com>
2024-08-14 14:36:37 +0530

    useState hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
 import { useState } from "react";
 import viteLogo from "/vite.svg";
+import Chai from "./Chai";
 
 function App() {
-  const [count, setCount] = useState(0);
+  let [count, setCount] = useState(5);
+
+  const AddValue = () => {
+    if (count < 20) {
+      setCount(count + 1);
+    }
+  };
+
+  const RemoveValue = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  };
 
   return (
     <>
       <h1>Hello world</h1>
+      {/* <Chai /> */}
+
+      <h2>Counter value: {count}</h2>
+      <button onClick={AddValue}>Add Value</button>
+      <button onClick={RemoveValue}>Remove Value</button>
     </>
   );
 }
---
+++ src/Chai.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Chai() {
+  return (
+    <>
+      <h1>Hello Chai!</h1>
+    </>
+  );
+}
